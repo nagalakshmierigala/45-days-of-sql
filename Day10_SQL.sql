945a5a00bef4283e784ad07e324b409e753dbb5f
Nagalakshmi Reddy Erigala <114171858+nagalakshmierigala@users.noreply.github.com>
2024-03-30 20:41:57 -0500

    Add files via upload

---
+++ Day10_SQL.sql
@@ -0,0 +1,57 @@
+ -- Average Time of Process per Machine
+--  Table: Activity
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | machine_id     | int     |
+-- | process_id     | int     |
+-- | activity_type  | enum    |
+-- | timestamp      | float   |
+-- +----------------+---------+
+-- The table shows the user activities for a factory website.
+-- (machine_id, process_id, activity_type) is the primary key (combination of columns with unique values) of this table.
+-- machine_id is the ID of a machine.
+-- process_id is the ID of a process running on the machine with ID machine_id.
+-- activity_type is an ENUM (category) of type ('start', 'end').
+-- timestamp is a float representing the current time in seconds.
+-- 'start' means the machine starts the process at the given timestamp and 'end' means the machine ends the process at the given timestamp.
+-- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair.
+-- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+-- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+-- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+-- Return the result table in any order.
+-- The result format is in the following example.
+# Write your MySQL query statement below
+select a.machine_id, round(avg(b.timestamp - a.timestamp), 3) as processing_time 
+from activity a join activity b
+on a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end'
+group by 1;
+
+-- Employee Bonus
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | empId       | int     |
+-- | name        | varchar |
+-- | supervisor  | int     |
+-- | salary      | int     |
+-- +-------------+---------+
+-- empId is the column with unique values for this table.
+-- Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+-- Table: Bonus
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | empId       | int  |
+-- | bonus       | int  |
+-- +-------------+------+
+-- empId is the column of unique values for this table.
+-- empId is a foreign key (reference column) to empId from the Employee table.
+-- Each row of this table contains the id of an employee and their respective bonus.
+-- Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+-- Return the result table in any order.
+# Write your MySQL query statement below
+select e.name, b.bonus
+from employee e left join bonus b on e.empid = b.empid
+where b.bonus <1000 or bonus is null;
