27dd26115559f10844b1a75e3d0562a8e1c39046
Nagalakshmi Reddy Erigala <114171858+nagalakshmierigala@users.noreply.github.com>
2024-03-31 21:16:54 -0500

    Add files via upload

---
+++ Day11_SQL.sql
@@ -0,0 +1,96 @@
+-- Students and Examinations
+-- Table: Students
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | student_id    | int     |
+-- | student_name  | varchar |
+-- +---------------+---------+
+-- student_id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the ID and the name of one student in the school.
+-- Table: Subjects
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | subject_name | varchar |
+-- +--------------+---------+
+-- subject_name is the primary key (column with unique values) for this table.
+-- Each row of this table contains the name of one subject in the school.
+-- Table: Examinations
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | student_id   | int     |
+-- | subject_name | varchar |
+-- +--------------+---------+
+-- There is no primary key (column with unique values) for this table. It may contain duplicates.
+-- Each student from the Students table takes every course from the Subjects table.
+-- Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+# Write your MySQL query statement below
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
+
+
+-- Managers with at Least 5 Direct Reports
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+-- Write a solution to find managers with at least five direct reports.
+-- Return the result table in any order.
+# Write your MySQL query statement below
+Select E2.name as name
+from Employee as E1
+Join Employee as E2
+on E1.managerID = E2.id
+Group by E2.id
+having count(E2.id)>=5;
+
+
+-- Confirmation Rate
+-- Table: Signups
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- user_id is the column of unique values for this table.
+-- Each row contains information about the signup time for the user with ID user_id.
+-- Table: Confirmations
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- | action         | ENUM     |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+-- user_id is a foreign key (reference column) to the Signups table.
+-- action is an ENUM (category) of the type ('confirmed', 'timeout')
+-- Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. 
+-- Round the confirmation rate to two decimal places.
+-- Write a solution to find the confirmation rate of each user.
+-- Return the result table in any order.
+# Write your MySQL query statement below
+SELECT a.user_id, round(ifnull(avg(action = 'confirmed'), 0),2) as confirmation_rate
+FROM Signups a
+LEFT JOIN Confirmations b
+ON a.user_id = b.user_id
+GROUP BY a.user_id;
